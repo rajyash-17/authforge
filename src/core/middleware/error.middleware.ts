769e9a35a682d570cf80bf7d8ad85b1dec519ce4
rajyash-17 <btech15103.24@bitmesra.ac.in>
2026-07-03 20:08:42 +0530

    feat(core): format zod validation errors

--- src/core/middleware/error.middleware.ts
+++ src/core/middleware/error.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 import { AppError } from "../errors/app-error";
+import { ZodError } from "zod";
 
 export function errorHandler(
     err: Error,
@@ -8,6 +9,18 @@ export function errorHandler(
     res: Response,
     next: NextFunction
 ) {
+    if (err instanceof ZodError) {
+        return res.status(400).json({
+            success: false,
+            error: {
+            message: "Validation Failed",
+            fields: err.issues.map((issue) => ({
+                field: issue.path.join("."),
+                message: issue.message,
+            })),
+            },
+        });
+    }
     if (err instanceof AppError) {
     return res.status(err.statusCode).json({
         success: false,
