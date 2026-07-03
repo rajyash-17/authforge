588b5ac0ec2014c868f27633ac649224afbdf78f
rajyash-17 <btech15103.24@bitmesra.ac.in>
2026-07-03 19:40:27 +0530

    refactor: add async handler middleware

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import {validate} from "../../core/middleware/validate.middleware"
 import { signupSchema } from "./auth.validation";
 import { authController } from "./auth.controller";
+import { asyncHandler } from "../../core/middleware/async-handler";
 
 const router=Router();
 
-router.post("/signup",validate(signupSchema),authController.signup);
+router.post("/signup",validate(signupSchema),asyncHandler(authController.signup));
 
 export default router;
