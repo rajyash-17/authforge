d253b3a0ea83349c1c844a94d1869fa04a3f7293
rajyash-17 <btech15103.24@bitmesra.ac.in>
2026-07-03 16:29:36 +0530

    feat: check duplicate email during signup

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,12 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
+import * as schema from "./schema/users";
+
 const connectionString = process.env.DATABASE_URL!;
 
 const client = postgres(connectionString);
 
-export const db = drizzle(client);
+export const db = drizzle(client,{
+    schema,
+});
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,20 @@
+
+import {eq} from "drizzle-orm";
+
+import {db} from '../../db';
+import { users } from "../../db/schema/users";
+
 import type { SignupInput } from "./auth.validation";
 
 export class AuthService{
-    async signup(data:SignupInput){}
+    async signup(data:SignupInput){
+        const existingUser=await db.query.users.findFirst({
+            where: eq(users.email,data.email)
+        });
+        if(existingUser){
+            throw new Error("Email already Exists");
+        }
+    }
 }
 
 export const authService=new AuthService();
