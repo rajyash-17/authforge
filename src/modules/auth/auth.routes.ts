186b1e79e19cef8f5ba21f780cddd8e427578333
rajyash-17 <btech15103.24@bitmesra.ac.in>
2026-07-03 17:23:49 +0530

    feat: implement auth routes

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { authController } from "./auth.controller";
+
+const router=Router();
+
+router.post("/signup",authController.signup);
+
+export default router;
